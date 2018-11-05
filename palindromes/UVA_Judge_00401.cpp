a8f67f6f49989d9220f94fe0de72141526920952
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2018-11-05 16:06:16 -0300

    UVA 00401 - Palindromes

--- palindromes/UVA_Judge_00401.cpp
+++ palindromes/UVA_Judge_00401.cpp
@@ -6,7 +6,7 @@ int main(){
 	string aux;
 	
 	string s1="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-    string s2="A   3  HIL JM O   2TUVWXY51SE Z  8 ";
+    	string s2="A   3  HIL JM O   2TUVWXY51SE Z  8 ";
 	map<char, char> m;
 	
 	for(int x = 0; x < s1.length(); x++) m[s1[x]] = s2[x];
