18df1d33733776cfb46ce9ab34a6116b7d23f390
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2018-11-06 06:41:53 -0300

    UVA 11332 - Summing Digits

--- adhocs/UVA_Judge_11332.cpp
+++ adhocs/UVA_Judge_11332.cpp
@@ -1,20 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int superdigit(long long n){
-	int calc = 0;
-	while(n != 0){
-		calc += n % 10;
-		n = n / 10;
-	}
-	if(calc < 10) return calc;
-	else superdigit(calc);
-}
-
 int main(){
-	long long n;
+	string n;
 	while(cin >> n){
-		if(n == 0) break;
-		cout << superdigit(n) << endl;
+		if(n == "0") break;
+		while(1){
+			int calc = 0;
+			for(int x = 0; x < n.length(); x++){
+				calc += n[x] - 48;
+			}
+			if(calc < 10){
+				cout << calc << endl;
+				break;
+			}else{
+				n = to_string(calc);
+			}
+		}
 	}
+
 }
