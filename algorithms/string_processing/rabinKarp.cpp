51b8bd4c833cb5075b82504b4016ef87654fcb88
Edson Neto <72220739+emn2@users.noreply.github.com>
2022-10-09 16:54:32 -0300

    Rabin Karp - Hash

---
+++ algorithms/string_processing/rabinKarp.cpp
@@ -0,0 +1,43 @@
+struct RabinKarp {
+private:
+    string s;
+    int n, base, mod;
+    vector<long> p, h;
+
+public:
+    RabinKarp(string &s, int base = 243, int mod = 1e9 + 7) {
+        this->s = s;
+        this->n = s.size();
+
+        this->base = base;
+        this->mod = mod;
+
+        build();
+    }
+
+    RabinKarp(int n, string &s, int base = 243, int mod = 1e9 + 7) {
+        this->s = s;
+        this->n = n;
+
+        this->base = base;
+        this->mod = mod;
+
+        build();
+    }
+
+    int hash(int l, int r) {
+        if (!l) return h[r];
+        return (h[r] - ((h[l - 1] * p[r - l + 1]) % mod) + mod) % mod;
+    }
+
+private:
+    void build() {
+        p.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+            p[i] = (p[i - 1] * base) % mod;
+
+        h.assign(n, s[0]);
+        for (int i = 1; i < n; i++)
+            h[i] = (h[i - 1] * base + s[i]) % mod;
+    }
+};
